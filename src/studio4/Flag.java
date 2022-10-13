0603e8f8599859c0f9934c012e9da81deb48b0c5
njpra <njpra@DESKTOP-ICABMGJ.wusm-wifi.wucon.wustl.edu>
2022-10-13 12:14:29 -0500

    flags

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,7 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.ellipse(0.5, 0.5, 0.4, 0.4);
+		StdDraw.filledRectangle(0.5, 0.5, 0.1, 0.2);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,47 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = 0;
+		double y = 0;
+		double halfH = 0;
+		double halfW = 0;
+		double[] xArray = new double[3];
+		double[] yArray = new double[3];
+		int N = 0;
+		if (shapeType.equals("rectangle") || shapeType.equals("ellipse")) {
+			x  = in.nextDouble();
+			y  = in.nextDouble();
+			halfW  = in.nextDouble();
+			halfH  = in.nextDouble();
+		} else if (shapeType.equals("triangle")) {
+			while (in.hasNext() == true) {
+				xArray[N] = in.nextDouble();
+				yArray[N] = in.nextDouble();
+				N++;
+			}
+		}
+		StdDraw.setPenColor(red, green, blue);
+		if (filled == true) {
+			if (shapeType.equals("rectangle")) {
+				StdDraw.filledRectangle(x, y, halfW, halfH);
+			} else if (shapeType.equals("ellipse")) {
+				StdDraw.filledEllipse(x, y, halfW, halfH);
+			} else if (shapeType.equals("triangle")) {
+				StdDraw.filledPolygon(xArray, yArray);
+			}
+		} else if (filled == false) {
+			if (shapeType.equals("rectangle")) {
+				StdDraw.rectangle(x, y, halfW, halfH);
+			} else if (shapeType.equals("ellipse")) {
+				StdDraw.ellipse(x, y, halfW, halfH);
+			} else if (shapeType.equals("triangle")) {
+				StdDraw.polygon(xArray, yArray);
+			}
+		}
 	}
 }
